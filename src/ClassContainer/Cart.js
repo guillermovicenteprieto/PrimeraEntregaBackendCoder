bd8a8202328eb4adbb5c75a1e100cce8dd87075e
Guillermo Vicente <84589575+guillermovicenteprieto@users.noreply.github.com>
2022-04-05 23:35:36 -0300

    Primer entrega Proyecto Final, Entrega 1

--- src/ClassContainer/Cart.js
+++ src/ClassContainer/Cart.js
@@ -7,9 +7,15 @@ class Cart {
         this.path = "./src/db/carts.txt";
         this.cart = [];
         this.id = 1;
-        this.product = {
+        this.cartProducts = {
             id: this.id,
             timestamp: moment().format("L LTS"),
+            name: "",
+            description: "",
+            code: "",
+            img: "",
+            price: 0,
+            stock: 0,
         };
     }
 
@@ -21,13 +27,13 @@ class Cart {
                 this.cart = JSON.parse(data);
                 if (this.cart.length > 0) {
                     this.id = parseInt(this.cart[this.cart.length - 1].id) + 1;
-                } 
+                }
             }
             return this.cart;
         } catch (error) {
             if (error.code === "ENOENT") {
                 await fs.writeFile(this.path, JSON.stringify([], null, 2));
-                return 'No hay carritos aún';
+                return 'No hay carrito';
             } else {
                 throw new Error((
                     "Se produjo un error en CartList: " + error.message
@@ -39,188 +45,115 @@ class Cart {
     async newCart() {
         try {
             const data = await this.cartList();
+            if (!data) {
+                data = this.cart;
+            } else if (data.length > 0) {
+                this.id = parseInt(data[data.length - 1].id) + 1;
+            } 
+            //this.id = 1;
             const cart = {
-                id: this.id,
-                timestamp: moment().format("L LTS"),
-                // products: [{
-                //     ...this.product,
-                // }],
-            };
-            data.push(cart);
-            await fs.writeFile(this.path, JSON.stringify(data, null, 2));
-            //this.id++;
-            return cart;
-        } catch (error) {
-            throw new Error((
-                "Se produjo un error en newCart: " + error.message
-            ));
-        }
+            id: this.id,
+            timestamp: moment().format("L LTS"),
+            products: [{
+                ...this.cartProducts,
+            }],
+        };
+        data.push(cart);
+        await fs.writeFile(this.path, JSON.stringify(data, null, 2));
+        return cart;
+    } catch(error) {
+        throw new Error((
+            "Se produjo un error en newCart: " + error.message
+        ));
     }
+}
 
     async deleteCart(id) {
-        try {
-            const data = await this.cartList();
-            const index = data.findIndex((cart) => cart.id === parseInt(id));
-            if (index === -1) {
-                throw new Error("Cart not found");
-            }
-            data.splice(index, 1);
-            await fs.writeFile(this.path, JSON.stringify(data, null, 2));
-            return data;
-        } catch (error) {
-            throw new Error((
-                "Se produjo un error en deleteCart: " + error.message
-            ));
+    try {
+        const data = await this.cartList();
+        const index = data.findIndex((cart) => cart.id === parseInt(id));
+        if (index === -1) {
+            throw new Error("Cart not found");
         }
+        data.splice(index, 1);
+        await fs.writeFile(this.path, JSON.stringify(data, null, 2));
+        return data;
+    } catch (error) {
+        throw new Error((
+            "Se produjo un error en deleteCart: " + error.message
+        ));
     }
+}
 
     async cartProducts(id) {
-        try {
-            const data = await this.cartList();
-            const cart = data.find((cart) => cart.id === parseInt(id));
-            if (!cart) {
-                throw new Error("Cart not found");
-            }
-            return cart.products;
-        } catch (error) {
-            throw new Error((
-                "Se produjo un error en cartProducts: " + error.message
-            ));
+    try {
+        const data = await this.cartList();
+        const cart = data.find((cart) => cart.id == parseInt(id));
+        if (!cart) {
+            throw new Error("Cart not found");
         }
+        return cart.products;
+
+    } catch (error) {
+        throw new Error((
+            "Se produjo un error en cartProducts: " + error.message
+        ));
     }
+}
 
     async addProduct(id, product) {
-        try {
-            const data = await this.cartList();
-            const cart = data.find((cart) => cart.id === parseInt(id));
-            if (!cart) {
-                throw new Error("Cart not found");
-            }
-            product = {
-                id: this.id,
-                timestamp: moment().format("L LTS"),
-                name: product.name,
-                description: product.description,
-                code: product.code,
-                img: product.img,
-                price: product.price,
-                stock: product.stock,
-            };
-            cart.products.push(product);
-            await fs.writeFile(this.path, JSON.stringify(data, null, 2));
-            return cart;
-        } catch (error) {
-            throw new Error((
-                "Se produjo un error en addProduct: " + error.message
-            ));
+    try {
+        const data = await this.cartList();
+        const cart = data.find((cart) => cart.id === parseInt(id));
+        if (!cart) {
+            cart = await this.newCart();
         }
+        product = {
+            id: this.id,
+            timestamp: moment().format("L LTS"),
+            name: product.name,
+            description: product.description,
+            code: product.code,
+            img: product.img,
+            price: product.price,
+            stock: product.stock,
+        };
+        cart.products.push(product);
+        await fs.writeFile(this.path, JSON.stringify(data, null, 2));
+        return cart;
+    } catch (error) {
+        throw new Error((
+            "Se produjo un error en addProduct: " + error.message
+        ));
     }
+}
 
-    async removeProduct(id, productId) {
-        try {
-            const data = await this.cartList();
-            const cart = data.find((cart) => cart.id === parseInt(id));
-            if (!cart) {
-                throw new Error("Cart not found");
-            }
-            const index = cart.products.findIndex(
-                (product) => product.id === parseInt(productId.id)
-            );
-            if (index === -1) {
+    async removeProduct(id, idProd) {
+    try {
+        const data = await this.cartList();
+        const cart = data.find((cart) => cart.id === parseInt(id));
+        if (cart) {
+
+            const cartIndex = cart.products.findIndex((product) => product.id == parseInt(id));
+            const product = cart.products.findIndex((product) => product.id == parseInt(idProd));
+            if (product != -1) {
                 throw new Error("Product not found");
             }
-            cart.products.splice(index, 1);
+            cart.products.splice(product, 1);
+            data[cartIndex] = cart;
+
             await fs.writeFile(this.path, JSON.stringify(data, null, 2));
             return cart;
-        } catch (error) {
-            throw new Error((
-                "Se produjo un error en removeProduct: " + error.message
-            ));
+        } else {
+            throw new Error("Cart not found");
         }
+    } catch (error) {
+        throw new Error((
+            "Se produjo un error en removeProduct: " + error.message
+        ));
     }
 
 }
+}
 
 module.exports = Cart;
-
-
-
-
-
-//   async save(product) {
-//     try {
-//       const data = await this.cartList();
-//       product = {
-//         id: this.id,
-//         timestamp: moment().format("L LTS"),
-//         name: product.name,
-//         description: product.description,
-//         code: product.code,
-//         img: product.img,
-//         price: product.price,
-//         stock: product.stock,
-//       };
-//       data.push(product);
-//       await fs.writeFile(this.path, JSON.stringify(data, null, 2));
-//       this.id++;
-//       return product;
-//     } catch (error) {
-//       throw error;
-//     }
-//   }
-
-//   async getById(id) {
-//     try {
-//       const data = await this.cartList();
-//       const product = data.find((product) => product.id == id);
-//       return product;
-//     } catch (error) {
-//       throw error;
-//     }
-//   }
-
-//   async update(id, product) {
-//     try {
-//       const data = await this.cartList();
-//       const index = data.findIndex((product) => product.id == id);
-//       data[index] = {
-//         id: this.id,
-//         timestamp: moment().format("L LTS"),
-//         name: product.name,
-//         description: product.description,
-//         code: product.code,
-//         img: product.img,
-//         price: product.price,
-//         stock: product.stock,
-//       };
-//       await fs.writeFile(this.path, JSON.stringify(data, null, 2));
-//       this.id++;
-//       return product;
-//     } catch (error) {
-//       throw error;
-//     }
-//   }
-
-//   async delete(id) {
-//     try {
-//       const data = await this.cartList();
-//       const index = data.findIndex((product) => product.id == id);
-//       data.splice(index, 1);
-//       await fs.writeFile(this.path, JSON.stringify(data, null, 2));
-//       return data;
-//     } catch (error) {
-//       throw error;
-//     }
-//   }
-
-//   async deleteAll() {
-//     try {
-//       const data = await this.cartList();
-//       data.splice(0, data.length);
-//       await fs.writeFile(this.path, JSON.stringify(data, null, 2));
-//       return data;
-//     } catch (error) {
-//       throw error;
-//     }
-//   }
-
--- src/routers/cartRouter.js
+++ src/routers/cartRouter.js
@@ -7,54 +7,11 @@ const cartManager = new Cart();
 const Products = require("../ClassContainer/Products");
 const productsManager = new Products();
 
-//GET: '/' -  Obtener todos los productos del carrito
-cartRouter.get("/", async (req, res) => {
-    try {
-        const cart = await cartManager.cartList();
-        res.status(200).json(cart);
-    } catch (error) {
-        res.status(500).json({
-            error: -1,
-            description: error.message,
-            status: 500
-        });
-    }
-});
-
-//GET: '/:id/productos' - listar todos los productos guardados en el carrito
-cartRouter.get("/:id/products", async (req, res) => {
-    try {
-        const cart = await cartManager.cartProducts(req.params.id);
-        res.status(200).json(cart);
-    } catch (error) {
-        res.status(500).json({
-            error: -1,
-            description: error.message,
-            status: 500
-        });
-    }
-});
-
 //POST: '/' - Crea un carrito y devuelve su id.
 cartRouter.post("/", async (req, res) => {
     try {
-        const newCart = await cartManager.newCart();
+        const newCart = await cartManager.newCart(); 
         res.status(200).json(newCart);
-    } catch (error) {
-        res.status(500).json({
-            error: -1,
-            description: error.message,
-            status: 500
-        });
-    }
-});
-
-//POST: '/:id/productos' - incorporar productos al carrito por su id de producto
-cartRouter.post("/:id/products", async (req, res) => {
-    try {
-        const product = await productsManager.save(req.body);
-        const cart = await cartManager.addProduct(req.params.id, product);
-        res.status(200).json(cart);
     } catch (error) {
         res.status(500).json({
             error: -1,
@@ -78,11 +35,57 @@ cartRouter.delete("/:id", async (req, res) => {
     }
 });
 
-//DELETE: '/:id/productos/:productId' - Eliminar un producto del carrito por su id de carrito y de producto
-cartRouter.delete("/:id/products/:productId", async (req, res) => {
+//GET: '/' -  Obtener todos los productos del carrito
+cartRouter.get("/", async (req, res) => {
     try {
-        const cart = await cartManager.removeProduct(req.params.id, req.params.productId);
+        const cart = await cartManager.cartList();
         res.status(200).json(cart);
+    } catch (error) {
+        res.status(500).json({
+            error: -1,
+            description: error.message,
+            status: 500
+        });
+    }
+});
+
+//GET: '/:id/productos' - listar todos los productos guardados en el carrito
+cartRouter.get("/:id/productos", async (req, res) => {
+    try {
+        const cart = await cartManager.cartProducts(req.params.id);
+        res.status(200).json(cart);
+    } catch (error) {
+        res.status(500).json({
+            error: -1,
+            description: error.message,
+            status: 500
+        });
+    }
+});
+
+//POST: '/:id/productos' - incorporar productos al carrito por su id de producto
+cartRouter.post("/:id/productos", async (req, res) => {
+    try {
+        const product = await productsManager.getById(req.body.id)
+        const addToCart = await cartManager.addProduct(req.params.id, product);
+        // const prodInCart = await cartManager.cartProducts(req.params.id);
+        // cart.push(prodInCart);
+        //const cart = await cartManager.addProduct(req.params.id, product);
+        res.status(200).json(addToCart);
+    } catch (error) {
+        res.status(500).json({
+            error: -1,
+            description: error.message,
+            status: 500
+        });
+    }
+});
+
+//DELETE: '/:id/productos/:id_prod' - Eliminar un producto del carrito por su id de carrito y de producto
+cartRouter.delete("/:id/productos/:idProd", async (req, res) => {
+    try {
+        const cartDeleted = await cartManager.removeProduct(req.params.id, req.params.idProd);
+        res.status(200).json(cartDeleted);
     } catch (error) {
         res.status(500).json({
             error: -1,
